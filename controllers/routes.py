c9a16986302fa6469a1b51c79f211d5444e7d58b
AV <av@admindeMBP.lan>
2017-09-04 17:14:26 +0800

    feat:增加了音乐的搜索和电视剧搜索API

--- controllers/routes.py
+++ controllers/routes.py
@@ -2,6 +2,7 @@
 from controllers.singer import singer_blueprint, SingerController
 from controllers.album import album_blueprint, AlbumController
 from controllers.song import song_blueprint, SongController
+from controllers.tv import tv_blueprint, TVController
 
 
 def init_app(current_app):
@@ -11,3 +12,5 @@ def init_app(current_app):
     current_app.register_blueprint(album_blueprint, url_prefix='/api/album')
     song_blueprint.add_url_rule('/search', 'search_api', SongController.search, methods=['get', 'post'])
     current_app.register_blueprint(song_blueprint, url_prefix='/api/song')
+    tv_blueprint.add_url_rule('/search', 'search_api', TVController.search, methods=['get', 'post'])
+    current_app.register_blueprint(tv_blueprint, url_prefix='/api/tv')
